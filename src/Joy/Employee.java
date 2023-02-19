0a278d1e50f4f46a16bcd514499e03504bd3d753
JACKSON DSOUZA <JACKSON DSOUZA@LAPTOP-TGLS3ILH>
2023-02-19 12:14:37 +0530

    second commit

--- src/Joy/Employee.java
+++ src/Joy/Employee.java
@@ -1,5 +1,37 @@
 package Joy;
 
+
 public class Employee {
+
+	int employeeId;
+	String employeeName;
+	
+	void setEmployeeDetails(int id,String name)
+	{
+		
+		employeeId=id;
+		employeeName=name;
+	}
+	int getEmployeeId()
+	{
+		return employeeId;
+	}
+	String getEmployeeName()
+	{
+		return employeeName;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e1=new Employee();
+		e1.setEmployeeDetails(23, "John");
+		
+		System.out.println(e1.getEmployeeId());
+		System.out.println(e1.getEmployeeName());
+		
+		
+
+	}
 
 }
